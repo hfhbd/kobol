ed9f6b07ffa27f973b834008dafa3fce5fe70cb9
hfhbd <hfhbd@users.noreply.github.com>
2022-05-20 18:34:45 +0200

    Add CI and detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.6.21" apply false
     id("org.jetbrains.intellij") version "1.5.3" apply false
     id("org.jetbrains.grammarkit") version "2021.2.2" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.20.0"
 }
 
 group = "app.softwork"
