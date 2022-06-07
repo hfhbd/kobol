5e44e5c41031bc28fff416c7560b3d2788fc6eb5
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-06-07 21:31:39 +0200

    Fix detekt (#6)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,58 +1,54 @@
+import io.gitlab.arturbosch.detekt.*
+
 plugins {
     kotlin("jvm") version "1.6.21" apply false
     id("org.jetbrains.intellij") version "1.6.0" apply false
     id("org.jetbrains.grammarkit") version "2021.2.2" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.20.0"
     `maven-publish`
 }
+group = "app.softwork"
 
 allprojects {
-    group = "app.softwork"
-    version = "1.0-SNAPSHOT"
-
     repositories {
         mavenCentral()
         maven(url = "https://www.jetbrains.com/intellij-repository/releases")
         maven(url = "https://cache-redirector.jetbrains.com/intellij-dependencies")
     }
-    plugins.apply("org.jetbrains.kotlin.jvm")
-    plugins.apply("org.gradle.maven-publish")
 
-    val emptyJar by tasks.creating(Jar::class) { }
+    configurations.all {
+        exclude(group = "com.jetbrains.rd")
+        exclude(group = "com.github.jetbrains", module = "jetCheck")
+        exclude(group = "org.roaringbitmap")
+    }
+}
 
-    group = "app.softwork"
+detekt {
+    source = files(rootProject.rootDir)
+    parallel = true
+    buildUponDefaultConfig = true
+}
 
-    publishing {
-        publications.create<MavenPublication>("mavenJava") {
-            from(components["java"])
-        }
-        publications.all {
-            this as MavenPublication
-            artifact(emptyJar) {
-                classifier = "javadoc"
-            }
-            pom {
-                name.set("app.softwork UUID Library")
-                description.set("A multiplatform Kotlin CSV and FLF serialization library")
-                url.set("https://github.com/hfhbd/kotlinx-serialization-csv")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("hfhbd")
-                        name.set("Philip Wedemann")
-                        email.set("mybztg+mavencentral@icloud.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git://github.com/hfhbd/kotlinx-serialization-csv.git")
-                    developerConnection.set("scm:git://github.com/hfhbd/kotlinx-serialization-csv.git")
-                    url.set("https://github.com/hfhbd/kotlinx-serialization-csv")
-                }
-            }
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.20.0")
+}
+
+tasks {
+    fun SourceTask.config() {
+        include("**/*.kt")
+        exclude("**/*.kts")
+        exclude("**/resources/**")
+        exclude("**/generated/**")
+        exclude("**/build/**")
+    }
+    withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
+        config()
+    }
+    withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        config()
+
+        reports {
+            sarif.required.set(true)
         }
     }
 }
--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -1,11 +1,15 @@
+plugins {
+    kotlin("jvm")
+}
+
 val idea = "211.7628.21"
 
 dependencies {
     implementation(projects.lexerParser)
 
-    compileOnly("com.jetbrains.intellij.java:java-psi:$idea")
-    compileOnly("com.jetbrains.intellij.platform:core-impl:$idea")
-    compileOnly("com.jetbrains.intellij.platform:core-ui:$idea")
-    compileOnly("com.jetbrains.intellij.platform:lang-impl:$idea")
+    implementation("com.jetbrains.intellij.java:java-psi:$idea")
+    implementation("com.jetbrains.intellij.platform:core-impl:$idea")
+    implementation("com.jetbrains.intellij.platform:core-ui:$idea")
+    implementation("com.jetbrains.intellij.platform:lang-impl:$idea")
     implementation("com.squareup:kotlinpoet:1.11.0")
 }
