beee48411f78125a1a56d6235b27d3721d9f5dc8
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-08-18 19:49:16 +0200

    Add generated folder to sourceSet (#133)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/KobolGradlePlugin.kt
+++ kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/KobolGradlePlugin.kt
@@ -1,8 +1,9 @@
 package app.softwork.kobol
 
 import org.gradle.api.*
+import org.gradle.api.tasks.*
 
-class KobolGradlePlugin: Plugin<Project> {
+class KobolGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.tasks.register("convertCobolToKotlin", KobolTask::class.java)
         val upload = target.tasks.register("uploadCobol", UploadTask::class.java) {
@@ -11,6 +12,11 @@ class KobolGradlePlugin: Plugin<Project> {
         target.tasks.register("buildCobol", BuildTask::class.java) {
             it.dependsOn(upload)
             it.group = "kobol"
+        }
+        target.plugins.withId("org.jetbrains.kotlin.jvm") {
+            val srcSet = target.extensions.findByType(SourceSetContainer::class.java)!!.getByName("main")
+            val kotlin = srcSet.extensions.getByName("kotlin") as org.gradle.api.file.SourceDirectorySet
+            kotlin.srcDir("build/generated/kobol")
         }
     }
 }
