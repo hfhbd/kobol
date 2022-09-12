88b836bf68dcd1e5375ab046dad33e43f57feb95
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-09-12 18:29:28 +0200

    Less dependencies (#155)
    * Remove com.jetbrains.intellij.platform:core-ui package
    
    * Remove com.jetbrains.intellij.platform:project-model dependency
    
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- kobol-lexer-parser/build.gradle.kts
+++ kobol-lexer-parser/build.gradle.kts
@@ -16,18 +16,21 @@ val grammar = configurations.create("grammar") {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.4.0")
     grammar("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
     compileOnly("com.jetbrains.intellij.java:java-psi:$idea")
     compileOnly("com.jetbrains.intellij.platform:core-impl:$idea")
-    compileOnly("com.jetbrains.intellij.platform:core-ui:$idea")
-    compileOnly("com.jetbrains.intellij.platform:lang-impl:$idea")
+    compileOnly("com.jetbrains.intellij.platform:util-ui:$idea")
+    compileOnly("com.jetbrains.intellij.platform:project-model-impl:$idea")
+    compileOnly("com.jetbrains.intellij.platform:analysis-impl:$idea")
 
     testImplementation(kotlin("test"))
+
     testImplementation("com.jetbrains.intellij.java:java-psi:$idea")
     testImplementation("com.jetbrains.intellij.platform:core-impl:$idea")
-    testImplementation("com.jetbrains.intellij.platform:core-ui:$idea")
-    testImplementation("com.jetbrains.intellij.platform:project-model:$idea")
+    testImplementation("com.jetbrains.intellij.platform:util-ui:$idea")
     testImplementation("com.jetbrains.intellij.platform:project-model-impl:$idea")
     testImplementation("com.jetbrains.intellij.platform:analysis-impl:$idea")
+
     testImplementation(projects.kobolFir)
 }
 
