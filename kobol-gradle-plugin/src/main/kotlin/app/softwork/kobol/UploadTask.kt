f215698ce56dbfb5e734728f9c99efcaa9888001
hfhbd <hfhbd@users.noreply.github.com>
2022-10-19 08:56:06 +0200

    Fix incremental upload

--- kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/UploadTask.kt
+++ kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/UploadTask.kt
@@ -7,8 +7,9 @@ import org.gradle.api.provider.*
 import org.gradle.api.tasks.*
 import org.gradle.api.tasks.PathSensitivity.*
 import org.gradle.work.*
+import java.io.*
 
-@DisableCachingByDefault
+@CacheableTask
 abstract class UploadTask : DefaultTask(), SshTask {
     init {
         group = "kobol"
@@ -17,7 +18,6 @@ abstract class UploadTask : DefaultTask(), SshTask {
     @get:Incremental
     @get:PathSensitive(RELATIVE)
     @get:InputFiles
-    @get:IgnoreEmptyDirectories
     abstract val files: ConfigurableFileCollection
 
     @get:Optional
@@ -45,12 +45,29 @@ abstract class UploadTask : DefaultTask(), SshTask {
     @get:InputFiles
     abstract val mvsFiles: ConfigurableFileCollection
 
+    @get:OutputDirectory
+    abstract val uploaded: DirectoryProperty
+
+    init {
+        uploaded.convention(project.layout.buildDirectory.dir("kobol/uploaded"))
+    }
+
+    @get:Input
+    @get:Optional
+    abstract val notTagged: ListProperty<String>
+
+    init {
+        notTagged.convention(listOf("jar"))
+    }
+
     @TaskAction
     fun execute(inputChanges: InputChanges) {
         val encoding = encoding.get()
         val copyToMVS = mvsFolder.orNull
         val keepUTF8 = keepUTF8.get()
         val folder = folder.get()
+        val uploaded = uploaded.get().asFile
+        val notTagged: List<String> = notTagged.get()
 
         sshClient {
             newSFTPClient().use { sftp ->
@@ -62,16 +79,24 @@ abstract class UploadTask : DefaultTask(), SshTask {
                     val file = change.file
                     val target = "$folder/${file.name}"
 
-                    sftp.put(FileSystemFile(file), target)
-                    exec("iconv -T -f utf-8 -t $encoding $target > $target.conv")
-                    if (keepUTF8) {
-                        exec("mv $target $target.utf8")
+                    try {
+                        sftp.put(FileSystemFile(file), target)
+                    } catch (_: IOException) {
+                        sftp.put(FileSystemFile(file), target)
                     }
-                    exec("mv $target.conv $target")
+                    if (file.extension !in notTagged) {
+                        exec("iconv -T -f utf-8 -t $encoding $target > $target.conv")
+
+                        if (keepUTF8) {
+                            exec("mv $target $target.utf8")
+                        }
+                        exec("mv $target.conv $target")
+                    }
                     if (copyToMVS != null && file in mvsFiles) {
                         val mvsName = file.nameWithoutExtension.uppercase()
                         exec("""cp $target "//'$copyToMVS($mvsName)'" """)
                     }
+                    file.copyTo(uploaded, true)
                 }
             }
         }
