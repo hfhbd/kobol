e68116db4313daf72d91af3e7c766b9a84170c5a
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2022-10-23 02:35:50 +0200

    Add sql support in Gradle plugin (#190)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/KobolGradlePlugin.kt
+++ kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/KobolGradlePlugin.kt
@@ -4,6 +4,7 @@ import org.gradle.api.*
 
 class KobolGradlePlugin : Plugin<Project> {
     override fun apply(target: Project) {
+        target.plugins.apply("org.gradle.idea")
         val convert = target.tasks.register("convertCobolToKotlin", KobolTask::class.java)
         val convertJava = target.tasks.register("convertCobolToJava", JavaKobolTask::class.java)
         target.plugins.withId("org.jetbrains.kotlin.jvm") {
