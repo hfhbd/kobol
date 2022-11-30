435df07d9baccec10c76270de1f80833461cf876
hfhbd <hfhbd@users.noreply.github.com>
2022-11-30 13:15:38 +0100

    Remove not working public kobolVersion

--- kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/gradle/Module.kt
+++
@@ -1,5 +0,0 @@
-package app.softwork.kobol.gradle
-
-import org.gradle.api.artifacts.dsl.DependencyHandler
-
-public fun DependencyHandler.kobol(module: String): String = "app.softwork:kobol-$module:$kobolVersion"
