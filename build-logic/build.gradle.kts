170d6588ff0412bec590f576d1e6e2c1838295c5
hfhbd <hfhbd@users.noreply.github.com>
2022-12-30 15:48:57 +0100

    Use java toolchain

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -12,6 +12,12 @@ dependencies {
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
 }
 
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 gradlePlugin {
     plugins {
         create("MyRepos") {
