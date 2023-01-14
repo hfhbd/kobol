d3a49e0b475508fc8b7ec55895f438b17e97e07a
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-01-15 00:00:46 +0100

    Test named accessors (#287)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/gradle/KobolGradlePlugin.kt
+++ kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/gradle/KobolGradlePlugin.kt
@@ -1,10 +1,17 @@
 package app.softwork.kobol.gradle
 
 import org.gradle.api.*
+import org.gradle.api.Named
 import org.gradle.api.plugins.JavaPlugin.*
+import javax.inject.*
 
 public class KobolGradlePlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        val kobol = project.extensions.create("kobol", Kobol::class.java)
+        kobol.cobolSources.register("testing.cbl")
+        kobol.cobolSources.register("main.cbl")
+        kobol.cobolSources.register("hello.cbl")
+
         val kobolIrPlugin = project.configurations.register("kobolIrPlugin") {
             it.isCanBeResolved = true
             it.isCanBeConsumed = false
@@ -18,7 +25,7 @@ public class KobolGradlePlugin : Plugin<Project> {
         val kobolFlowGraphPlugin = project.configurations.register("kobolFlowGraphPlugin") {
             it.isCanBeResolved = true
             it.isCanBeConsumed = false
-            
+
             it.defaultDependencies {
                 it.add(project.dependencies.create("app.softwork:kobol-plugins-flow-graph-plantuml:$kobolVersion"))
             }
@@ -54,3 +61,24 @@ public class KobolGradlePlugin : Plugin<Project> {
         project.tasks.register("cleanCobol", CleanCobol::class.java)
     }
 }
+
+public interface Kobol {
+    public val cobolSources: NamedDomainObjectContainer<CobolSource>
+}
+
+public abstract class CobolSource @Inject constructor(name: String, project: Project) : Named {
+    private val name = name.toLowerCase().split(".").first()
+    private val nameTitle = this.name.let {
+        it[0].toUpperCase() + it.substring(1, it.length)
+    }
+
+    override fun getName(): String = name
+    private val plugins = project.configurations.register("kobol${nameTitle}Plugin") {
+        it.isCanBeResolved = true
+        it.isCanBeConsumed = false
+
+        it.defaultDependencies {
+            it.add(project.dependencies.create("app.softwork:kobol-plugins-flow-graph-plantuml:$kobolVersion"))
+        }
+    }
+} 
