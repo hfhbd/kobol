50cb9687170a25b10aff06b23ed4faf8003195b1
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-02-01 13:40:35 +0100

    Fix Tasks (#312)
    * Try fixing classloader
    
    * Try fixing classloader
    
    * Fix tasks overloads
    
    ---------
    
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/gradle/CobolSource.kt
+++ kobol-gradle-plugin/src/main/kotlin/app/softwork/kobol/gradle/CobolSource.kt
@@ -5,6 +5,8 @@ import org.gradle.api.Named
 import org.gradle.api.artifacts.*
 import org.gradle.api.artifacts.dsl.*
 import org.gradle.api.file.*
+import org.gradle.api.tasks.*
+import org.gradle.kotlin.dsl.*
 import javax.inject.*
 
 public abstract class CobolSource @Inject constructor(
@@ -13,26 +15,44 @@ public abstract class CobolSource @Inject constructor(
     project: Project
 ) : Named, FileSystemLocation by file {
     private val name = name.lowercase()
-    private val nameTitle = this.name.replaceFirstChar {
-        it.titlecaseChar()
-    }
-    public val taskName: String = "convert${nameTitle}Cobol"
+    private val nameTitle = this.name.nameTitle()
+    public val taskName: String = this.name.taskName()
 
     override fun getName(): String = name
 
-    public val plugins: NamedDomainObjectProvider<Configuration> = project.configurations.register("kobol${nameTitle}Plugin") {
-        isCanBeResolved = true
-        isCanBeConsumed = false
-        isVisible = false
-    }
-    
+    public val plugins: NamedDomainObjectProvider<Configuration> =
+        project.configurations.register("kobol${nameTitle}Plugin") {
+            isCanBeResolved = true
+            isCanBeConsumed = false
+            isVisible = false
+        }
+
     public fun DependencyHandler.plugin(dependency: Any): Dependency? {
         return add(plugins.name, dependency)
     }
+
+    public fun TaskContainer.convert(): TaskProvider<KobolTask> = named<KobolTask>(taskName)
+    public fun TaskContainer.convert(configuration: Action<KobolTask>) {
+        named<KobolTask>(taskName).configure(configuration)
+    }
 }
+
+private fun String.nameTitle() = replaceFirstChar {
+    it.titlecaseChar()
+}
+private fun String.taskName() = "convert${nameTitle()}Cobol"
+
 
 public fun DependencyHandler.kobolPlugin(source: NamedDomainObjectProvider<CobolSource>, dependency: Any) {
     source.configure {
         plugin(dependency)
     }
+}
+
+public fun TaskContainer.convert(source: NamedDomainObjectProvider<CobolSource>): TaskProvider<KobolTask> {
+    return named<KobolTask>(source.name.taskName())
+}
+
+public fun TaskContainer.convert(source: NamedDomainObjectProvider<CobolSource>, configuration: Action<KobolTask>) {
+    return named<KobolTask>(source.name.taskName()).configure(configuration)
 }
