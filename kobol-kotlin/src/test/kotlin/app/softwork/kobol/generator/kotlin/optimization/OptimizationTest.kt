16852eb29c26a034fca8a2c6322ddb4ff7e52bdc
hfhbd <hfhbd@users.noreply.github.com>
2023-02-03 00:38:37 +0100

    Add Java naming and keep naming plugins

--- kobol-kotlin/src/test/kotlin/app/softwork/kobol/generator/kotlin/optimization/OptimizationTest.kt
+++ kobol-kotlin/src/test/kotlin/app/softwork/kobol/generator/kotlin/optimization/OptimizationTest.kt
@@ -149,7 +149,7 @@ class OptimizationTest {
     }
 
     @Test
-    fun camelCase() {
+    fun javaNames() {
         //language=COBOL
         val input = """
         123456 IDENTIFICATION              DIVISION.
@@ -164,13 +164,13 @@ class OptimizationTest {
         123456 PROCEDURE                   DIVISION.
         123456     DISPLAY HELLO WORLD.
         123456
-        123456 FOO SECTION.
+        123456 FOO-BAR SECTION.
         123456* Some Comment
         123456     MOVE "42" TO WORLD
         123456     DISPLAY "ANSWER"WORLD.
         """.trimIndent()
             .toIR()
-            .let { CamelCase()(it, emptyList()) }.single()
+            .let { JavaNames()(it, emptyList()) }.single()
 
         val output = generate(input)
 
@@ -181,7 +181,7 @@ class OptimizationTest {
         import kotlin.String
         import kotlin.Unit
         
-        public fun foo(): Unit {
+        public fun fooBar(): Unit {
           // Some Comment
           world = "42"
           println("ANSWER${'$'}world")
@@ -193,7 +193,59 @@ class OptimizationTest {
         
         public fun main(): Unit {
           println("${'$'}hello${'$'}world")
-          foo()
+          fooBar()
+        }
+
+        """.trimIndent()
+        assertEquals(expected, output.toString())
+    }
+    
+    @Test
+    fun keepNames() {
+        //language=COBOL
+        val input = """
+        123456 IDENTIFICATION              DIVISION.
+        123456 PROGRAM-ID.                 HELLO.
+        123456 AUTHOR. WEDEMANN / Softwork.app
+        123456 INSTALLATION. Softwork.app
+        123456 DATE-WRITTEN TODAY.
+        123456 DATA                        DIVISION.
+        123456 WORKING-STORAGE SECTION.
+        123456 77 WORLD PIC X(6) VALUE 'WORLD!'.
+        123456 77 HELLO PIC X(6) VALUE 'HELLO'.
+        123456 PROCEDURE                   DIVISION.
+        123456     DISPLAY HELLO WORLD.
+        123456
+        123456 FOO-BAR SECTION.
+        123456* Some Comment
+        123456     MOVE "42" TO WORLD
+        123456     DISPLAY "ANSWER"WORLD.
+        """.trimIndent()
+            .toIR()
+            .let { KeepNames()(it, emptyList()) }.single()
+
+        val output = generate(input)
+
+        //language=kotlin
+        val expected = """
+        package hello
+        
+        import kotlin.String
+        import kotlin.Unit
+        
+        public fun FOO_BAR(): Unit {
+          // Some Comment
+          WORLD = "42"
+          println("ANSWER${'$'}WORLD")
+        }
+        
+        public var WORLD: String = "WORLD!"
+        
+        public var HELLO: String = "HELLO"
+        
+        public fun main(): Unit {
+          println("${'$'}HELLO${'$'}WORLD")
+          FOO_BAR()
         }
 
         """.trimIndent()
