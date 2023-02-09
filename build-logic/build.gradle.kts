b65083e8a9f0a7f507fb0465b771eb9d42265449
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-02-09 09:34:06 +0100

    Update build.gradle.kts

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin")
     implementation("org.jetbrains.kotlin:kotlin-serialization:$kotlin")
     implementation("app.cash.licensee:licensee-gradle-plugin:1.7.0-SNAPSHOT")
-    implementation("org.jetbrains.intellij:org.jetbrains.intellij.gradle.plugin:1.12.3-SNAPSHOT")
+    implementation("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.12.3-SNAPSHOT")
     implementation("org.jetbrains.intellij.plugins:gradle-grammarkit-plugin:2022.3")
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
     implementation("app.softwork:serviceloader-gradle-plugin:0.0.2")
