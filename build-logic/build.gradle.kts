96567b89aa08890ce74640ac6e4fe68782810308
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-02-25 16:26:47 +0100

    Bump Shadow to 8.0 and fix non cacheable shadow jar (#375)
    Fix non cacheable jar
    
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation("app.cash.licensee:licensee-gradle-plugin:1.7.0-SNAPSHOT")
     implementation("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.13.0")
     implementation("org.jetbrains.intellij.plugins:gradle-grammarkit-plugin:2022.3.1")
-    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
+    implementation("gradle.plugin.com.github.johnrengelman:shadow:8.0.0")
     implementation("app.softwork:serviceloader-gradle-plugin:0.0.2")
 }
 
