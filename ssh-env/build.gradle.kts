b6cc5016a92df3e6f70f96a0caa97fd9dc44c0e4
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-02-27 15:46:06 +0100

    Try fixing ssh jna dependency (#384)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- ssh-env/build.gradle.kts
+++ ssh-env/build.gradle.kts
@@ -1,21 +1,16 @@
 plugins {
     setup
+    repos
 }
 
 dependencies {
     api("com.hierynomus:sshj:0.35.0")
-    api("com.jcraft:jsch.agentproxy.sshj:0.0.9") // remove stupid open net.schmizz:sshj:[0.8.1,)
-    api("com.jcraft:jsch.agentproxy.pageant:0.0.9")
+    api("com.jcraft:jsch.agentproxy.sshj:0.0.12")
+    api("com.jcraft:jsch.agentproxy.pageant:0.0.12")
 }
 
 licensee {
     allow("MIT")
-    allowDependency("net.java.dev.jna", "jna", "4.1.0") {
-        because("Apache-2.0")
-    }
-    allowDependency("net.java.dev.jna", "jna-platform", "4.1.0") {
-        because("Apache-2.0")
-    }
     allowUrl("http://www.jcraft.com/jsch-agent-proxy/LICENSE.txt") // BSD
     allowUrl("http://www.jcraft.com/jzlib/LICENSE.txt") // BSD
     allowUrl("https://www.bouncycastle.org/licence.html") // MIT
