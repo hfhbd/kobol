3f76945a1fb75c7a0cd1048c50a52bb3e06b473e
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-02-27 11:43:32 +0100

    Don't upgrade JNA in SSH task (#383)
    Fix ssh jna dependency
    
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- ssh-env/build.gradle.kts
+++ ssh-env/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 }
 
 dependencies {
-    api("net.java.dev.jna:jna-platform:5.13.0")
     api("com.hierynomus:sshj:0.35.0")
     api("com.jcraft:jsch.agentproxy.sshj:0.0.9") // remove stupid open net.schmizz:sshj:[0.8.1,)
     api("com.jcraft:jsch.agentproxy.pageant:0.0.9")
@@ -11,6 +10,12 @@ dependencies {
 
 licensee {
     allow("MIT")
+    allowDependency("net.java.dev.jna", "jna", "4.1.0") {
+        because("Apache-2.0")
+    }
+    allowDependency("net.java.dev.jna", "jna-platform", "4.1.0") {
+        because("Apache-2.0")
+    }
     allowUrl("http://www.jcraft.com/jsch-agent-proxy/LICENSE.txt") // BSD
     allowUrl("http://www.jcraft.com/jzlib/LICENSE.txt") // BSD
     allowUrl("https://www.bouncycastle.org/licence.html") // MIT
