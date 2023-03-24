8a7613f176ce8e3a13700b1e3a860e97fd6efbe5
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-03-24 10:26:55 +0100

    Use proper version catalog (#400)
    * Use proper version catalog
    
    * Use proper version catalog
    
    * Use proper version catalog
    
    ---------
    
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- build-logic/src/main/kotlin/kotlinSetup.gradle.kts
+++ build-logic/src/main/kotlin/kotlinSetup.gradle.kts
@@ -1,39 +1,14 @@
-import org.jetbrains.kotlin.gradle.dsl.*
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 plugins {
     app.cash.licensee
-    `maven-publish`
+    id("publish")
 }
 
 publishing {
-    repositories {
-        maven(url = "https://maven.pkg.github.com/hfhbd/kobol") {
-            name = "GitHubPackages"
-            credentials(PasswordCredentials::class)
-        }
-    }
     if (name != "gradle-plugin") {
         publications.register<MavenPublication>("mavenJava") {
             from(components["java"])
-        }
-    }
-    publications.configureEach {
-        this as MavenPublication
-        pom {
-            name.set("app.softwork KOBOL")
-            url.set("https://github.com/hfhbd/kobol")
-            developers {
-                developer {
-                    id.set("hfhbd")
-                    name.set("Philip Wedemann")
-                    email.set("mybztg+mavencentral@icloud.com")
-                }
-            }
-            scm {
-                connection.set("scm:git://github.com/hfhbd/kobol.git")
-                developerConnection.set("scm:git://github.com/hfhbd/kobol.git")
-                url.set("https://github.com/hfhbd/kobol")
-            }
         }
     }
 }
@@ -57,9 +32,4 @@ pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
 
 licensee {
     allow("Apache-2.0")
-}
-
-tasks.withType<AbstractArchiveTask>().configureEach {
-    isPreserveFileTimestamps = false
-    isReproducibleFileOrder = true
 }
---
+++ catalog/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    `version-catalog`
+    publish
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            from(components["versionCatalog"])
+        }
+    }
+}
+
+catalog {
+    versionCatalog {
+        val version = version("kobol", project.version.toString())
+        plugin("kobol", "app.softwork.kobol").versionRef(version)
+
+        fun add(name: String, module: String = name) {
+            library("kobol-$name", "app.softwork.kobol", module).versionRef(version)
+        }
+
+        fun plugin(module: String) {
+            add(name = "plugin-$module", module = "plugin-$module")
+        }
+
+        add("f-3.1")
+
+        add("ir")
+        add("fir")
+
+        add("kotlin")
+        add("kotlin-sqldelight")
+        add("kotlin-kotlinxserialization")
+        add("kotlin-filejava", "kotlin-file-java")
+
+        add("java")
+        add("java-java8")
+        add("java-jdbc")
+
+        plugin("booleanexpressions")
+        plugin("javanames")
+        plugin("keepnames")
+        plugin("constvariables")
+        plugin("flow-graph-plantuml")
+        plugin("ifassignments")
+        plugin("inlining")
+        plugin("nullabletozero")
+        plugin("objects")
+        plugin("optimize")
+        plugin("private")
+        plugin("readonlyvariables")
+        plugin("statistic")
+        plugin("useparameters")
+        plugin("exitprocess")
+        plugin("nosynthetic")
+    }
+}
