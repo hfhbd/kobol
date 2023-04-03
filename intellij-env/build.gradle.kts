07b373dff1d8efc9d5129893a95a752071d50d36
hfhbd <hfhbd@users.noreply.github.com>
2023-04-03 18:23:20 +0200

    a

--- intellij-env/build.gradle.kts
+++ intellij-env/build.gradle.kts
@@ -14,11 +14,7 @@ configurations.implementation {
 
 val idea = "231.8109.197"
 dependencies {
-    shade("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    shade("com.jetbrains.intellij.platform:core-impl:$idea") {
-        // https://youtrack.jetbrains.com/issue/IDEA-301677
-        exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-coroutines-core")
-    }
+    shade("com.jetbrains.intellij.platform:core-impl:$idea")
     shade("com.jetbrains.intellij.platform:project-model-impl:$idea")
     shade("com.jetbrains.intellij.platform:analysis-impl:$idea")
     shade("com.jetbrains.intellij.platform:indexing-impl:$idea")
@@ -29,16 +25,17 @@ configurations.configureEach {
     exclude(group = "com.jetbrains.rd")
     exclude(group = "com.github.jetbrains", module = "jetCheck")
     exclude(group = "com.jetbrains.intellij.platform", module = "wsl-impl")
+    exclude(group = "com.jetbrains.intellij.platform", module = "diagnostic-telemetry")
+    exclude(group = "com.jetbrains.intellij.platform", module = "statistics")
     exclude(group = "com.jetbrains.infra")
     exclude(group = "org.roaringbitmap")
     exclude(group = "ai.grazie.spell")
     exclude(group = "ai.grazie.model")
     exclude(group = "ai.grazie.utils")
     exclude(group = "ai.grazie.nlp")
-}
-
-tasks.jar {
-    enabled = false
+    exclude(group = "jaxen")
+    exclude(group = "com.h2database")
+    exclude("com.jetbrains", "jbr-api")
 }
 
 tasks.shadowJar {
@@ -56,6 +53,7 @@ tasks.shadowJar {
     include("com/github/benmanes/**")
 
     include("messages/*.properties")
+    include("kotlinx/**")
     include("gnu/**")
 
     exclude("/kotlin/**")
@@ -68,6 +66,7 @@ artifacts {
 
 licensee {
     allow("BSD-2-Clause")
+    allow("BSD-3-Clause")
     allowDependency("oro", "oro", "2.0.8") {
         because("Apache-2-0")
     }
@@ -76,16 +75,12 @@ licensee {
     }
     allowUrl("http://www.opensource.org/licenses/zlib-license.html") // BSD
 
-    allowDependency("org.jetbrains.intellij.deps.fastutil", "intellij-deps-fastutil", "8.5.6-10") {
+    allowDependency("org.jetbrains.intellij.deps.fastutil", "intellij-deps-fastutil", "8.5.11-18") {
         because("Apache-2.0")
     }
     allowDependency("org.jetbrains.intellij.deps", "java-compatibility", "1.0.1") {
         because("Apache-2.0")
     }
-    allowDependency("org.jetbrains.intellij.deps", "jdom", "2.0.6") {
-        because("Apache-2.0")
-    }
-    allowUrl("https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html")
 
     allowDependency("com.jetbrains.intellij.platform", "analysis", idea)
     allowDependency("com.jetbrains.intellij.platform", "analysis-impl", idea)
@@ -103,8 +98,6 @@ licensee {
     allowDependency("com.jetbrains.intellij.platform", "jps-model", idea)
     allowDependency("com.jetbrains.intellij.platform", "jps-model-impl", idea)
     allowDependency("com.jetbrains.intellij.platform", "jps-model-serialization", idea)
-    allowDependency("com.jetbrains.intellij.platform", "object-serializer", idea)
-    allowDependency("com.jetbrains.intellij.platform", "object-serializer-annotations", idea)
     allowDependency("com.jetbrains.intellij.platform", "project-model", idea)
     allowDependency("com.jetbrains.intellij.platform", "project-model-impl", idea)
     allowDependency("com.jetbrains.intellij.platform", "util", idea)
@@ -112,12 +105,15 @@ licensee {
     allowDependency("com.jetbrains.intellij.platform", "util-class-loader", idea)
     allowDependency("com.jetbrains.intellij.platform", "util-diff", idea)
     allowDependency("com.jetbrains.intellij.platform", "util-ex", idea)
+    allowDependency("com.jetbrains.intellij.platform", "util-jdom", idea)
     allowDependency("com.jetbrains.intellij.platform", "util-rt", idea)
     allowDependency("com.jetbrains.intellij.platform", "util-rt-java8", idea)
     allowDependency("com.jetbrains.intellij.platform", "util-text-matching", idea)
+    allowDependency("com.jetbrains.intellij.platform", "util-trove", idea)
     allowDependency("com.jetbrains.intellij.platform", "util-ui", idea)
     allowDependency("com.jetbrains.intellij.platform", "util-xml-dom", idea)
     allowDependency("com.jetbrains.intellij.platform", "util-zip", idea)
+    allowDependency("com.jetbrains.intellij.platform", "workspace-model-jps", idea)
     allowDependency("com.jetbrains.intellij.platform", "workspace-model-storage", idea)
 }
 
