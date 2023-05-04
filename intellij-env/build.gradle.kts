82fd7926315b72cedcc472b8a3049b12c64a7bf7
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-05-04 12:48:52 +0200

    Reduce intellij dependencies (#430)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- intellij-env/build.gradle.kts
+++ intellij-env/build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
 val idea = "221.6008.13"
 dependencies {
     shadow("com.jetbrains.intellij.platform:core-impl:$idea")
-    shadow("com.jetbrains.intellij.platform:util-ui:$idea")
     shadow("com.jetbrains.intellij.platform:project-model-impl:$idea")
     shadow("com.jetbrains.intellij.platform:analysis-impl:$idea")
     shadow("com.jetbrains.intellij.platform:indexing-impl:$idea")
@@ -19,12 +18,40 @@ configurations.shadow {
     exclude(group = "com.jetbrains.rd")
     exclude(group = "com.github.jetbrains", module = "jetCheck")
     exclude(group = "com.jetbrains.intellij.platform", module = "wsl-impl")
+    exclude(group = "com.jetbrains.intellij.platform", module = "util-text-matching")
+    exclude(group = "com.jetbrains.intellij.platform", module = "util-diff")
+    exclude(group = "com.jetbrains.intellij.platform", module = "util-xml-dom")
+    exclude(group = "com.jetbrains.intellij.platform", module = "util-zip")
+    exclude(group = "com.jetbrains.intellij.platform", module = "code-style")
+    exclude(group = "com.jetbrains.intellij.platform", module = "editor")
+    exclude(group = "com.jetbrains.intellij.platform", module = "editor-ex")
+    exclude(group = "com.jetbrains.intellij.platform", module = "jps-model-serialization")
+    exclude(group = "com.jetbrains.intellij.platform", module = "object-serializer")
+    exclude(group = "com.jetbrains.intellij.platform", module = "ide-util-io")
     exclude(group = "com.jetbrains.infra")
     exclude(group = "org.roaringbitmap")
     exclude(group = "ai.grazie.spell")
     exclude(group = "ai.grazie.model")
     exclude(group = "ai.grazie.utils")
     exclude(group = "ai.grazie.nlp")
+    exclude(group = "oro")
+    exclude(group = "org.lz4")
+    exclude(group = "com.fasterxml")
+    exclude(group = "be.cyberelf.nanoxml")
+    exclude(group = "com.esotericsoftware.kryo")
+    exclude(group = "dk.brics")
+    exclude(group = "net.jcip")
+    exclude(group = "org.jetbrains.intellij.deps", module = "trove4j")
+    exclude(group = "org.jetbrains.intellij.deps", module = "java-compatibility")
+    exclude(group = "org.jetbrains.intellij.deps.batik")
+    exclude(group = "com.google.guava")
+    exclude(group = "com.google.code.gson")
+    exclude(group = "io.netty")
+    exclude(group = "com.github.ben-manes.caffeine")
+    exclude(group = "org.objenesis")
+    exclude(group = "one.util", module = "streamex")
+    exclude(group = "xml-apis")
+    exclude(group = "org.imgscalr")
 }
 
 tasks.shadowJar {
@@ -63,6 +90,7 @@ configurations {
 }
 
 val licenseeShadow by tasks.registering(LicenseeTask::class) {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
     configurationToCheck(configurations.shadow.get())
     outputDir.set(reporting.baseDirectory.dir("licenseeShadow"))
 }
@@ -78,56 +106,43 @@ afterEvaluate {
 }
 
 licensee {
-    allow("BSD-2-Clause")
-    allowDependency("oro", "oro", "2.0.8") {
-        because("Apache-2-0")
-    }
-    allowDependency("net.jcip", "jcip-annotations", "1.0") {
-        because("Apache-2.0")
-    }
-    allowUrl("http://www.opensource.org/licenses/zlib-license.html") // BSD
-
     allowDependency("org.jetbrains.intellij.deps.fastutil", "intellij-deps-fastutil", "8.5.6-10") {
-        because("Apache-2.0")
-    }
-    allowDependency("org.jetbrains.intellij.deps", "java-compatibility", "1.0.1") {
         because("Apache-2.0")
     }
     allowDependency("org.jetbrains.intellij.deps", "jdom", "2.0.6") {
         because("Apache-2.0")
     }
-    allowUrl("https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html")
 
-    allowDependency("com.jetbrains.intellij.platform", "analysis", idea)
-    allowDependency("com.jetbrains.intellij.platform", "analysis-impl", idea)
-    allowDependency("com.jetbrains.intellij.platform", "code-style", idea)
-    allowDependency("com.jetbrains.intellij.platform", "concurrency", idea)
-    allowDependency("com.jetbrains.intellij.platform", "core", idea)
-    allowDependency("com.jetbrains.intellij.platform", "core-impl", idea)
-    allowDependency("com.jetbrains.intellij.platform", "core-ui", idea)
-    allowDependency("com.jetbrains.intellij.platform", "editor", idea)
-    allowDependency("com.jetbrains.intellij.platform", "editor-ex", idea)
-    allowDependency("com.jetbrains.intellij.platform", "extensions", idea)
-    allowDependency("com.jetbrains.intellij.platform", "ide-util-io", idea)
-    allowDependency("com.jetbrains.intellij.platform", "indexing", idea)
-    allowDependency("com.jetbrains.intellij.platform", "indexing-impl", idea)
-    allowDependency("com.jetbrains.intellij.platform", "jps-model", idea)
-    allowDependency("com.jetbrains.intellij.platform", "jps-model-impl", idea)
-    allowDependency("com.jetbrains.intellij.platform", "jps-model-serialization", idea)
-    allowDependency("com.jetbrains.intellij.platform", "object-serializer", idea)
-    allowDependency("com.jetbrains.intellij.platform", "object-serializer-annotations", idea)
-    allowDependency("com.jetbrains.intellij.platform", "project-model", idea)
-    allowDependency("com.jetbrains.intellij.platform", "project-model-impl", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util-base", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util-class-loader", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util-diff", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util-ex", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util-rt", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util-rt-java8", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util-text-matching", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util-ui", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util-xml-dom", idea)
-    allowDependency("com.jetbrains.intellij.platform", "util-zip", idea)
-    allowDependency("com.jetbrains.intellij.platform", "workspace-model-storage", idea)
+    fun allowIdea(module: String) {
+        allowDependency("com.jetbrains.intellij.platform", module, idea) {
+            because("Apache-2.0")
+        }
+    }
+
+    allowIdea("analysis")
+    allowIdea("analysis-impl")
+    allowIdea("concurrency")
+    allowIdea("core")
+    allowIdea("core-impl")
+    allowIdea("core-ui")
+    allowIdea("extensions")
+    allowIdea("indexing")
+    allowIdea("indexing-impl")
+    allowIdea("jps-model")
+    allowIdea("jps-model-impl")
+    allowIdea("project-model")
+    allowIdea("project-model-impl")
+    allowIdea("util")
+    allowIdea("util-base")
+    allowIdea("util-class-loader")
+    allowIdea("util-ex")
+    allowIdea("util-rt")
+    allowIdea("util-rt-java8")
+    allowIdea("util-ui")
+    allowIdea("workspace-model-storage")
+}
+
+tasks.register("depsize", DepSize::class.java) {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    configurationToCheck.from(configurations.shadow)
 }
