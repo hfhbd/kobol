2e2d185a36938c0966b7a962f93d66402edb8123
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-06-16 21:32:27 +0200

    Revert "Remove unused repos" (#489)
    Revert "Remove unused repos (#488)"
    
    This reverts commit 69237cc0aca80b64ecab343eab09b1a3357da827.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,10 @@
 pluginManagement {
     includeBuild("build-logic")
+    repositories {
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+        gradlePluginPortal()
+    }
 }
 
 plugins {
