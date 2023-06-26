f9016b78ae4fe756b862f722c80128b6be259953
hfhbd <hfhbd@users.noreply.github.com>
2023-06-26 16:12:33 +0200

    Add optional default implementation of sqlca

--- lexer-parser/src/test/kotlin/app/softwork/kobol/CobolParserTest.kt
+++ lexer-parser/src/test/kotlin/app/softwork/kobol/CobolParserTest.kt
@@ -444,29 +444,6 @@ class CobolParserTest {
             123456 DISPLAY SQLSTATE.
         """.trimIndent()
 
-        val sqlca = """
-            01 SQLCA SYNC.
-                05 SQLCAID PIC X(8) VALUE "SQLCA   ".
-                05 SQLCABC PIC S9(9) COMP-5 VALUE 136.
-                05 SQLCODE PIC S9(9) COMP-5.
-                05 SQLERRM.
-                05 SQLERRP PIC X(8).
-                05 SQLERRD OCCURS 6 TIMES PIC S9(9) COMP-5.
-                05 SQLWARN.
-                    10 SQLWARN0 PIC X.
-                    10 SQLWARN1 PIC X.
-                    10 SQLWARN2 PIC X.
-                    10 SQLWARN3 PIC X.
-                    10 SQLWARN4 PIC X.
-                    10 SQLWARN5 PIC X.
-                    10 SQLWARN6 PIC X.
-                    10 SQLWARN7 PIC X.
-                    10 SQLWARN8 PIC X.
-                    10 SQLWARN9 PIC X.
-                    10 SQLWARNA PIC X.
-                05 SQLSTATE PIC X(5).
-        """.trimIndent()
-
         val lines = """
             123456 77 FOO PIC 9 VALUE 1.
         """.trimIndent()
@@ -554,7 +531,7 @@ class CobolParserTest {
                     +Display(Interpolation(NumberVariable(foo)))
                     +Display(StringVariable(sqlState))
                 })
-            ), input.toTree("SQLCA" to sqlca, "LINES" to lines)
+            ), input.toTree("LINES" to lines)
         )
     }
 
