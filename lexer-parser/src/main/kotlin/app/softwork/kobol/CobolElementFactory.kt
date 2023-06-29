1ee0c291d05bb25e36a3bab0c8f623be5644d76f
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-06-29 14:46:51 +0200

    Update CobolElementFactory.kt

--- lexer-parser/src/main/kotlin/app/softwork/kobol/CobolElementFactory.kt
+++ lexer-parser/src/main/kotlin/app/softwork/kobol/CobolElementFactory.kt
@@ -15,30 +15,33 @@ public object CobolElementFactory {
             PsiFileFactory.getInstance(project).createFileFromText(name, CobolFileType.INSTANCE, cobolText) as CobolFile
         return file.program
     }
-    
+
+    // https://www.ibm.com/docs/en/db2-for-zos/11?topic=sqlca-included#d449577e134
     private const val sqlca = """
-            01 SQLCA SYNC.
-                05 SQLCAID PIC X(8) VALUE "SQLCA   ".
-                05 SQLCABC PIC S9(9) COMP-5 VALUE 136.
-                05 SQLCODE PIC S9(9) COMP-5.
-                05 SQLERRM.
-                05 SQLERRP PIC X(8).
-                05 SQLERRD OCCURS 6 TIMES PIC S9(9) COMP-5.
-                05 SQLWARN.
-                    10 SQLWARN0 PIC X.
-                    10 SQLWARN1 PIC X.
-                    10 SQLWARN2 PIC X.
-                    10 SQLWARN3 PIC X.
-                    10 SQLWARN4 PIC X.
-                    10 SQLWARN5 PIC X.
-                    10 SQLWARN6 PIC X.
-                    10 SQLWARN7 PIC X.
-                    10 SQLWARN8 PIC X.
-                    10 SQLWARN9 PIC X.
-                    10 SQLWARNA PIC X.
-                05 SQLSTATE PIC X(5).
-            77 SQLSTATE PIC X(5).
-            77 SQLCODE  PIC S9(9) USAGE COMP.
+            01 SQLCA.
+   05 SQLCAID      PIC X(8).
+   05 SQLCABC      PIC S9(9) COMP-5.
+   05 SQLCODE      PIC S9(9) COMP-5.
+   05 SQLERRM.
+      49 SQLERRML  PIC S9(4) COMP-5.
+      49 SQLERRMC  PIC X(70).
+   05 SQLERRP      PIC X(8).
+   05 SQLERRD      OCCURS 6 TIMES
+                   PIC S9(9) COMP-5.
+   05 SQLWARN.
+      10 SQLWARN0  PIC X.
+      10 SQLWARN1  PIC X.
+      10 SQLWARN2  PIC X.
+      10 SQLWARN3  PIC X.
+      10 SQLWARN4  PIC X.
+      10 SQLWARN5  PIC X.
+      10 SQLWARN6  PIC X.
+      10 SQLWARN7  PIC X.
+   05 SQLEXT.
+      10 SQLWARN8  PIC X.
+      10 SQLWARN9  PIC X.
+      10 SQLWARNA  PIC X.
+      10 SQLSTATE  PIC X(5).
         """
 
     public fun includeSQL(project: Project, fileName: String): List<CobolRecordDef> {
