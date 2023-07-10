55eb74d92ebc25b3f31c8049f1eedb025d07cc47
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-07-10 12:51:40 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ pluginManagement {
     includeBuild("build-logic")
     repositories {
         mavenCentral()
-        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
         gradlePluginPortal()
     }
 }
