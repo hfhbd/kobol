76a6960b7415b6271df04773c235dbfff0c07496
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-07-10 12:51:24 +0200

    Update settings.gradle.kts

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -2,7 +2,6 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
-        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
         gradlePluginPortal()
     }
 
