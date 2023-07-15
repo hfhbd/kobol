cdcf9605055a4357df399772d4462fcbda542144
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-07-15 13:33:58 +0200

    Use variable for Language (#522)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- fir/src/main/kotlin/app/softwork/kobol/fir/InlineSqlFile.kt
+++ fir/src/main/kotlin/app/softwork/kobol/fir/InlineSqlFile.kt
@@ -2,16 +2,16 @@ package app.softwork.kobol.fir
 
 import com.alecstrong.sql.psi.core.*
 import com.intellij.icons.*
-import com.intellij.lang.Language
+import com.intellij.lang.*
 import com.intellij.openapi.fileTypes.*
 import com.intellij.psi.*
 
-internal class InlineSqlFile(viewProvider: FileViewProvider): SqlFileBase(viewProvider, SqlInlineLanguage) {
+internal class InlineSqlFile(viewProvider: FileViewProvider) : SqlFileBase(viewProvider, SqlInlineLanguage) {
     override val order: Int? = null
     override fun getFileType() = InlineSqlFileType
 }
 
-internal object SqlInlineLanguage: Language("SqlInlineLanguage")
+internal val SqlInlineLanguage = object : Language("SqlInlineLanguage") {}
 
 internal object InlineSqlFileType : LanguageFileType(SqlInlineLanguage) {
     override fun getIcon() = AllIcons.Providers.DB2
