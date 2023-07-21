34c1b60611285f3e5f03f273662b3a5f8d6b6fe5
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-07-21 15:00:43 +0200

    Builder: Add addAll (#536)

--- fir/src/main/kotlin/app/softwork/kobol/Builder.kt
+++ fir/src/main/kotlin/app/softwork/kobol/Builder.kt
@@ -5,7 +5,7 @@ package app.softwork.kobol
 @JvmInline
 public value class Builder<T> (private val list: MutableList<T> = mutableListOf()) : MutableList<T> by list {
     public inline operator fun T.unaryPlus(): Boolean = add(this)
-    //public inline operator fun List<T>.unaryPlus(): Boolean = addAll(this)
+    public inline operator fun List<T>.unaryPlus(): Boolean = addAll(this)
 }
 
 public inline fun <T> build(builder: Builder<T>.() -> Unit): MutableList<T> = Builder<T>().apply(builder).toMutableList()
