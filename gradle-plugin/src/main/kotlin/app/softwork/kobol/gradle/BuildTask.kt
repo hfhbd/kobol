306c6da8fd6472cb75a8db94a7b87a7d2e074fa5
hfhbd <hfhbd@users.noreply.github.com>
2023-10-05 10:40:44 +0200

    Remove hard-coded exports

--- gradle-plugin/src/main/kotlin/app/softwork/kobol/gradle/BuildTask.kt
+++ gradle-plugin/src/main/kotlin/app/softwork/kobol/gradle/BuildTask.kt
@@ -3,8 +3,4 @@ package app.softwork.kobol.gradle
 import org.gradle.work.*
 
 @DisableCachingByDefault
-public abstract class BuildTask : KobolRunTask() {
-    init {
-        export.set(listOf("PATH=${"$"}{PATH}:/usr/lpp/IBM/cobol/igyv6r3/bin"))
-    }
-}
+public abstract class BuildTask : KobolRunTask()
