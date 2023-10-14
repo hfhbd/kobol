9c5cdeb349433f74d1bb049153c7658a4245b8a7
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-10-14 14:02:18 +0200

    Add c89 and cob2 build cmd (#603)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- gradle-plugin/src/main/kotlin/app/softwork/kobol/gradle/BuildTask.kt
+++ gradle-plugin/src/main/kotlin/app/softwork/kobol/gradle/BuildTask.kt
@@ -1,6 +1,38 @@
 package app.softwork.kobol.gradle
 
+import org.gradle.api.provider.*
+import org.gradle.api.tasks.*
 import org.gradle.work.*
 
 @DisableCachingByDefault
-public abstract class BuildTask : KobolRunTask()
+public abstract class BuildTask : KobolRunTask() {
+    @get:Input
+    public abstract val c89Options: Property<String>
+
+    @get:Input
+    public abstract val c89: ListProperty<String>
+
+    @get:Input
+    public abstract val cob2Options: Property<String>
+
+    @get:Input
+    public abstract val cob2: ListProperty<String>
+
+    private fun Provider<List<String>>.options(options: Provider<String>): Provider<List<String>> =
+        zip(options) { list, options ->
+            list.map {
+                "$options $it"
+            }
+        }
+
+    private operator fun <T : Any> Provider<List<T>>.plus(other: Provider<List<T>>): Provider<List<T>> =
+        zip(other) { a, b ->
+            a + b
+        }
+
+    init {
+        val c89cmd = c89.options(c89Options)
+        val cob2cmd = cob2.options(cob2Options)
+        cmds.convention(c89cmd + cob2cmd)
+    }
+}
