ebf4094c5ff5d30e12004c12bb2543062eebf9da
hfhbd <hfhbd@users.noreply.github.com>
2023-10-16 14:47:06 +0200

    Fix Gradle inputs and ssh upload

--- ssh-env/build.gradle.kts
+++ ssh-env/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    api("com.hierynomus:sshj:0.36.0")
+    api("com.hierynomus:sshj:0.35.0")
 
     implementation("net.java.dev.jna:jna:5.13.0")
     implementation("net.java.dev.jna:jna-platform:5.13.0")
