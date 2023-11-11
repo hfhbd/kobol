c867c5530c90ac0b9142142b1c8f1b7d8c48a793
hfhbd <hfhbd@users.noreply.github.com>
2023-11-11 15:01:56 +0100

    Add error annotator

--- intellij-plugin/src/main/kotlin/app/softwork/kobol/intellij/CobolAnnotator.kt
+++ intellij-plugin/src/main/kotlin/app/softwork/kobol/intellij/CobolAnnotator.kt
@@ -8,7 +8,6 @@ import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.elementType
 import com.intellij.psi.util.parentOfType
 
 internal class CobolAnnotator : Annotator {
