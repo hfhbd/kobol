b0774c14dfa362126ce958a98db0362f4a3c405c
hfhbd <hfhbd@users.noreply.github.com>
2023-11-13 22:05:14 +0100

    Reproducer

--- intellij-plugin/build.gradle.kts
+++ intellij-plugin/build.gradle.kts
@@ -13,5 +13,4 @@ repositories {
     maven(url = "https://www.jetbrains.com/intellij-repository/releases")
     maven(url = "https://cache-redirector.jetbrains.com/intellij-dependencies")
     maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies/")
-
 }
