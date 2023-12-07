b54f82e3edcb3d7defec3a3f4f16a3604eb6c85b
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-12-07 23:30:10 +0100

    Revert "Lazy configure the kobolClasspath" (#644)
    Revert "Lazy configure the kobolClasspath (#643)"
    
    This reverts commit c17349ffc5e973bf47b4a242057181d29bbebea3.

--- gradle-plugin/src/main/kotlin/app.softwork.kobol.gradle.kts
+++ gradle-plugin/src/main/kotlin/app.softwork.kobol.gradle.kts
@@ -4,6 +4,16 @@ import org.gradle.api.tasks.*
 
 val kobol = configurations.dependencyScope("kobol")
 
+dependencies {
+    kobol("app.softwork.kobol:builder:$KOBOL_VERSION")
+    kobol("app.softwork.kobol:intellij-env:$KOBOL_VERSION")
+}
+
+val kobolClasspath = configurations.resolvable("kobolClasspath") {
+    // https://github.com/gradle/gradle/issues/26732
+    extendsFrom(kobol.get())
+}
+
 val uploadCobol by tasks.registering(UploadTask::class)
 val buildCobol by tasks.registering(BuildTask::class) {
     dependsOn(uploadCobol)
@@ -46,17 +56,6 @@ fun SourceSet.createConvertTask(uploadTask: TaskProvider<out UploadTask>): TaskP
     cobolSrc.destinationDirectory.convention(layout.buildDirectory.dir("generated/kobol/"))
 
     convert {
-
-        dependencies {
-            kobol("app.softwork.kobol:builder:$KOBOL_VERSION")
-            kobol("app.softwork.kobol:intellij-env:$KOBOL_VERSION")
-        }
-
-        val kobolClasspath = configurations.resolvable("kobolClasspath") {
-            // https://github.com/gradle/gradle/issues/26732
-            extendsFrom(kobol.get())
-        }
-
         classpath.from(kobolClasspath)
         sources.from(cobolSrc.sourceDirectories)
         outputFolder.convention(cobolSrc.destinationDirectory)
