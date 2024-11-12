46333261e204685237b55a3a856e1065b3062413
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-11-12 13:40:41 +0100

    Add visual guides in IDE for column 6, 7, 12, 80 by default

---
+++ intellij-plugin/src/main/kotlin/app/softwork/kobol/intellij/CobolCodeStyleSettings.kt
@@ -0,0 +1,12 @@
+package app.softwork.kobol.intellij
+
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings
+
+class CobolCodeStyleSettings(settings: CodeStyleSettings) : CustomCodeStyleSettings(
+    "CobolCodeStyleSettings", settings
+) {
+    init {
+        settings.defaultSoftMargins = listOf(6, 7, 12, 80)
+    }
+}
