e9bbf5d1d0d3d86d113644c6b37b293b47702429
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-12-21 20:11:29 +0100

    Only change the Visual Guides in cobol source files (#816)

--- intellij-plugin/src/main/kotlin/app/softwork/kobol/intellij/CobolCodeStyleSettings.kt
+++ intellij-plugin/src/main/kotlin/app/softwork/kobol/intellij/CobolCodeStyleSettings.kt
@@ -6,8 +6,4 @@ import com.intellij.psi.codeStyle.CustomCodeStyleSettings
 class CobolCodeStyleSettings(settings: CodeStyleSettings) : CustomCodeStyleSettings(
     "CobolCodeStyleSettings",
     settings,
-) {
-    init {
-        settings.defaultSoftMargins = listOf(6, 7, 12, 80)
-    }
-}
+)
