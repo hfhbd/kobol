00bcc10b921189d9ece35e618e46c969de166595
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-06-09 11:47:50 +0200

    Remove snapshot version of sql-psi

--- builder/build.gradle.kts
+++ builder/build.gradle.kts
@@ -10,5 +10,6 @@ dependencies {
     compileOnly(libs.bundles.idea)
 
     implementation(libs.db2.dialect)
+    implementation(libs.sql.psi.environment)
     testImplementation(libs.bundles.idea)
 }
