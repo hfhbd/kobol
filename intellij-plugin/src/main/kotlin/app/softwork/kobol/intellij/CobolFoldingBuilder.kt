d523968244b352c7fae4dec776fe7c37bc1ea84a
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-09-28 16:25:48 +0200

    Implement section folding

---
+++ intellij-plugin/src/main/kotlin/app/softwork/kobol/intellij/CobolFoldingBuilder.kt
@@ -0,0 +1,28 @@
+package app.softwork.kobol.intellij
+
+import app.softwork.kobol.CobolFile
+import app.softwork.kobol.CobolProcedureSection
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingBuilderEx
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.project.DumbAware
+import com.intellij.psi.PsiElement
+import com.intellij.util.containers.toArray
+
+class CobolFoldingBuilder : FoldingBuilderEx(), DumbAware {
+    override fun buildFoldRegions(
+        root: PsiElement,
+        document: Document,
+        quick: Boolean,
+    ): Array<FoldingDescriptor> = (root as CobolFile).program.procedureDiv.procedureSectionList.map {
+        FoldingDescriptor(
+            it.node,
+            it.textRange,
+        )
+    }.toArray(FoldingDescriptor.EMPTY_ARRAY)
+
+    override fun isCollapsedByDefault(node: ASTNode): Boolean = false
+
+    override fun getPlaceholderText(node: ASTNode): String? = (node.psi as? CobolProcedureSection)?.sectionID?.text?.let { "$it SECTION." }
+}
